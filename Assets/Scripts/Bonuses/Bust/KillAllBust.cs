ffbb4f6454bf39b4492cc1f7333ca5454ded09e8
EvaAvocado <2002mskirill2002@gmail.com>
2022-06-29 11:37:24 +0300

    Fix bust and add icon

--- Assets/Scripts/Bonuses/Bust/KillAllBust.cs
+++ Assets/Scripts/Bonuses/Bust/KillAllBust.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Data;
+using UIComponents;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace Bonuses
@@ -6,11 +8,17 @@ namespace Bonuses
     public class KillAllBust : Bust
     {
         [SerializeField] private UnityEvent _spawnText;
+        
         private GameObject[] _monsters;
+        private GameData _data;
+        private ScoreComponent _score;
+        private int _monstersCount;
 
         private void Awake()
         {
             _monsters = GameObject.FindGameObjectsWithTag("Monster");
+            _data = FindObjectOfType<GameData>();
+            _score = FindObjectOfType<ScoreComponent>();
         }
 
         protected override void Start()
@@ -22,13 +30,18 @@ namespace Bonuses
         [ContextMenu("BusterAction")]
         protected override void BusterAction()
         {
+            _monstersCount = 0;
             foreach (var monster in _monsters)
             {
                 if (monster != null)
                 {
+                    _monstersCount++;
                     Destroy(monster);
                 }
             }
+
+            _data.CurrentMonsters = 0;
+            _score.ChangeScore(_monstersCount); 
         }
     }
 }
